fac6b659991005592a9a84349c5130fdd87dda7d
Madsen <112688147+ollemadsen@users.noreply.github.com>
2022-12-13 16:48:04 +0100

    ss

--- Java/src/BookNow.java
+++ Java/src/BookNow.java
@@ -5,6 +5,9 @@ public class BookNow {
 
     JFrame frame = new JFrame("BookNowPage");
     JLabel chooseDestination = new JLabel("Choose your destination");
+    JLabel CphToLarnaca = new JLabel("CPH - Larnaca");
+
+
 
     ImageIcon image = new ImageIcon("waterfall1.jpg");
     BookNow(){
@@ -15,15 +18,19 @@ public class BookNow {
         chooseDestination.setForeground(Color.BLACK);
         chooseDestination.setText("Choose your destination");
 
+        CphToLarnaca.setBounds(100,200,75,25);
+
+
+
 
 
         chooseDestination.setIcon(image);
         frame.add(chooseDestination);
+        frame.add(CphToLarnaca);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800,800);
         frame.setLayout(null);
         frame.setVisible(true);
-
     }
 
 }
