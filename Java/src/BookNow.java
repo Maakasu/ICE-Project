8725bfc18a620b28ee89353d020be7079d54617e
Marcus Anastasiades <91838917+Maakasu@users.noreply.github.com>
2022-12-13 16:00:07 +0100

    changes

--- Java/src/BookNow.java
+++ Java/src/BookNow.java
@@ -1,24 +1,63 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class BookNow {
+public class BookNow implements ActionListener {
 
     JFrame frame = new JFrame("BookNowPage");
     JLabel chooseDestination = new JLabel("Choose your destination");
 
+    JLabel pickYourDestination = new JLabel("Choose your destination");
+    JButton cphLarnacaButton = new JButton("Copenhagen - Larnaca");
+
+    JButton cphSingaporeButton = new JButton("Copenhagen - Singapore ");
+
+    JButton cphNewYorkButton = new JButton("Copgenhagen - New York City");
+
+
+
+
     ImageIcon image = new ImageIcon("waterfall1.jpg");
     BookNow(){
+
+        pickYourDestination.setBounds(0,0,800,800);
+        pickYourDestination.setFont(new Font(null,Font.PLAIN,25));
+        pickYourDestination.setForeground(Color.black);
+        pickYourDestination.setText("Choose your destination");
+        pickYourDestination.setHorizontalTextPosition(JLabel.CENTER);
+        pickYourDestination.setVerticalTextPosition(JLabel.TOP);
+
 
 
         chooseDestination.setBounds(0,0,800,800);
         chooseDestination.setFont(new Font(null,Font.PLAIN,25));
         chooseDestination.setForeground(Color.BLACK);
-        chooseDestination.setText("Choose your destination");
 
 
+        cphLarnacaButton.setBounds(100,100,200,25);
+        cphLarnacaButton.setFocusable(false);
+        cphLarnacaButton.addActionListener(this);
+
+        cphSingaporeButton.setBounds(100,200,200,25);
+        cphSingaporeButton.setFocusable(false);
+        cphSingaporeButton.addActionListener(this);
+
+        cphNewYorkButton.setBounds(100,300,200,25);
+        cphNewYorkButton.setFocusable(false);
+        cphNewYorkButton.addActionListener(this);
+
+
+
+
+        frame.add(pickYourDestination);
+        frame.add(cphLarnacaButton);
+        frame.add(cphSingaporeButton);
+        frame.add(cphNewYorkButton);
+        frame.add(chooseDestination);
 
         chooseDestination.setIcon(image);
-        frame.add(chooseDestination);
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800,800);
         frame.setLayout(null);
@@ -26,6 +65,10 @@ public class BookNow {
 
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
 }
 
 
--- Java/src/Bookings.java
+++ Java/src/Bookings.java
@@ -3,22 +3,22 @@ import java.awt.*;
 
 public class Bookings {
 
-    JFrame frame = new JFrame("BookNowPage");
-    JLabel chooseDestination = new JLabel("Your Bookings:");
+    JFrame frame = new JFrame("YourBookings");
+    JLabel yourDestination = new JLabel("Your Bookings:");
 
     ImageIcon image = new ImageIcon("Plane-in-sky.jpg");
     Bookings(){
 
 
-        chooseDestination.setBounds(0,0,800,800);
-        chooseDestination.setFont(new Font(null,Font.PLAIN,25));
-        chooseDestination.setForeground(Color.BLACK);
+        yourDestination.setBounds(0,0,800,800);
+        yourDestination.setFont(new Font(null,Font.PLAIN,25));
+        yourDestination.setForeground(Color.BLACK);
 
 
 
 
-        chooseDestination.setIcon(image);
-        frame.add(chooseDestination);
+        yourDestination.setIcon(image);
+        frame.add(yourDestination);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800,800);
         frame.setLayout(null);
